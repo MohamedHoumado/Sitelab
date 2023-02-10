eecd65982594c8d70c72447df7687e84cd246dba
MohamedHoumado <mohamed.houmado.mhm@gmail.com>
2023-02-10 10:04:42 +0100

    code source

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import images from "./images.jpg"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenu</h1>
+      <img src={images} alt="images"/>
+    </div>
+  );
+}
+
+export default App;
